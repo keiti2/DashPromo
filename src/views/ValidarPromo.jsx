b55e506ce01881de4b3ebc41b1abd220d9539a5a
henrique <henrique.keiti2@gmail.com>
2019-11-03 21:26:52 -0300

    Inicio validacao cupom

--- src/views/ValidarPromo.jsx
+++ src/views/ValidarPromo.jsx
@@ -4,14 +4,23 @@ import axios from 'axios';
 import { Card } from "components/Card/Card.jsx";
 import { thArray } from "variables/Variables.jsx";
 import Button from "components/CustomButton/CustomButton.jsx";
+import  {Input}  from 'reactstrap';
+
 class ValidarPromo extends Component {
     constructor(props){
         super(props);
         this.state={
           sucesso:"True",
           promocao:[]  ,
-          value:0
+          value:0,
+          btnconfirmar:true,
+          codigocupom:"",
+          dadosCupom:[],
+          nomecliente:"",
+          qtde:0,
+          nomePromo:""
         }
+        this.validarCupom=this.validarCupom.bind(this)
       }
 
     componentDidMount() {
@@ -23,19 +32,40 @@ class ValidarPromo extends Component {
         )
       }
 
+validarCupom(){
+  axios.get(`http://52.67.233.156/api/cupom/`+ this.state.codigocupom)
+  .then(res => {
+    const dadosCupom = res.data;
+    this.setState({ dadosCupom });
+    if (this.state.dadosCupom.message=="Cupom"){
+      this.setState({nomecliente:dadosCupom.data.cliente.nome,nomePromo:dadosCupom.data.promocao.nomePromocao,qtde:dadosCupom.data.qtde})
+
+    }else{
+      alert("Codigo cupom inválido")
+    }
+  }
+  )
+  
+  console.log("Validou")
+}      
+
+onChange = e => {
+  this.setState({[e.target.name]: e.target.value})
+}
+
     render() {
         return (
-            <Grid >
+            <Grid fluid >
                 <br></br>
                 <Row>
-                    <div class="col-md-6" text-align="center">
+                    <div class="col-md-6">
                         <div class="card">
                         <div class="card-body">
                             <h5 class="card-title" text-align="center">Digite o código do cupom promocional</h5>
-                            <input type="text" class="form-control" placeholder="Código Cupom" aria-label="Username" aria-describedby="basic-addon1"></input>
+                            <Input type="text" name="codigocupom" id="codigocupom" placeholder="Código Cupom" onChange={this.onChange} value={ this.state.codigocupom} />
                             <br></br>
-                            <Button bsStyle="validar" pullRight fill   >
-                                Validar
+                            <Button  pullRight fill  onClick={this.validarCupom} >
+                                Validarrr
                             </Button>
                             
                         </div>
@@ -45,15 +75,58 @@ class ValidarPromo extends Component {
                         <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Dados Cupom:</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Cliente:</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Produto:</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">Quantidade:</h6>
-    
-                            <a href="#" class="btn btn-primary">Confirmar</a>
+                            <h6 class="card-subtitle mb-2 text-muted">Cliente: {this.state.nomecliente}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted">Produto: {this.state.nomePromo}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted">Quantidade: {this.state.qtde}</h6>
+                        <div display='none'>
+                            <Button  pullRight fill   >
+                                Confirmar
+                            </Button>
+                        </div>
                         </div>
                         </div>
                     </div>
                 </Row>
+
+                <Row>
+        <Col md={12}>
+              <Card
+                title="Promoções resgatadas"
+                category="Promoções resgatadas do dia"
+                ctTableFullWidth
+                ctTableResponsive
+                content={
+                  
+                  <Table striped hover>
+                    <thead>
+                      <tr>
+                        {thArray.map((prop, key) => {
+                          return <th key={key}>{prop}</th>;
+                        })}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      
+                     {this.state.promocao.map(promo=>
+                     {
+                       return(
+                        <tr key={promo.idPromocao}>
+                            <td >{promo.promocao.nomePromocao}</td>
+                            <td >{promo.qtde + " Uni"}</td>
+                            <td >{"R$ " + promo.promocao.valorPromocao}</td>
+                            <td >{promo.cliente.nome}</td>
+                          </tr>
+                       )
+                     }
+                      )} 
+                    </tbody>
+                  </Table>
+                }
+              />
+            </Col>
+        </Row>
+
+
                 </Grid>
             
 
